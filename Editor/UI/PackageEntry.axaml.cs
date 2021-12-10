472c2c4dbe0ff31f3fe21459f9bc1d9c4f0a2255
Massimo Bertozzi <maximumoverflow904@gmail.com>
2021-12-10 12:04:05 +0100

    Implemented NuGet package search.

---
+++ Editor/UI/PackageEntry.axaml.cs
@@ -0,0 +1,48 @@
+﻿using BladeEngine.Editor.UI.Models;
+using BladeEngine.Editor.NuGet;
+using Avalonia.Media.Imaging;
+using Avalonia.Markup.Xaml;
+using Avalonia.Controls;
+
+namespace BladeEngine.Editor.UI;
+
+public class PackageEntry : UserControl
+{
+	private readonly Image _image;
+	private readonly TextBlock _title;
+	private readonly ComboBox _versions;
+
+	public PackageEntry()
+	{
+		AvaloniaXamlLoader.Load(this);
+		_image = this.FindControl<Image>("Image");
+		_title = this.FindControl<TextBlock>("Title");
+		_versions = this.FindControl<ComboBox>("Versions");
+	}
+
+	private PackageEntry(ProjectModel model, NugetSearchResult metadata) : this()
+	{
+		DataContext = model;
+		_title.Text = metadata.Title;
+		_versions.Items = metadata.PackageVersions;
+		_versions.SelectedIndex = 0;
+		var path = $"Nuget/Cache/Icons/{metadata.Title}.png";
+		if(File.Exists(path)) _image.Source = new Bitmap(path);
+	}
+
+	public static async Task<PackageEntry> Create(ProjectModel model, NugetSearchResult metadata)
+	{
+		var iconPath = $"Nuget/Cache/Icons/{metadata.Title}.png";
+		if (!File.Exists(iconPath) && !string.IsNullOrEmpty(metadata.IconUrl))
+		{
+			Directory.CreateDirectory("Nuget/Cache/Icons/");
+			Debug.Log($"Caching icon for {metadata.Title} from {metadata.IconUrl}");
+			using var client = new HttpClient();
+			var response = await client.GetAsync(metadata.IconUrl);
+			var data = await response.Content.ReadAsByteArrayAsync();
+			await File.WriteAllBytesAsync(iconPath, data);
+		}
+
+		return new PackageEntry(model, metadata);
+	}
+}
--- Editor/UI/ProjectSettings.axaml.cs
+++ Editor/UI/ProjectSettings.axaml.cs
@@ -1,26 +1,33 @@
-﻿using Avalonia.Markup.Xaml;
-using Avalonia.Controls;
-using BladeEngine.Core;
+﻿using Avalonia;
 using BladeEngine.Editor.UI.Models;
+using NuGet.Protocol.Core.Types;
+using Avalonia.Markup.Xaml;
+using NuGet.Configuration;
+using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Media;
+using BladeEngine.Editor.NuGet;
+using NuGet.Common;
 
 namespace BladeEngine.Editor.UI;
 
 public class ProjectSettings : Window
 {
 	private readonly ComboBox _architecture;
+	private readonly StackPanel _packageList;
+	private readonly TextBox _packageSearch;
 	
 	public ProjectSettings()
 	{
 		AvaloniaXamlLoader.Load(this);
 		_architecture = this.FindControl<ComboBox>("Architecture");
+		_packageList = this.FindControl<StackPanel>("PackageList");
+		_packageSearch = this.FindControl<TextBox>("PackageSearch");
 	}
 
-	public ProjectSettings(ProjectModel project)
+	public ProjectSettings(ProjectModel project) : this()
 	{
 		DataContext = project;
-		AvaloniaXamlLoader.Load(this);
-		
-		_architecture = this.FindControl<ComboBox>("Architecture");
 		foreach (ComboBoxItem i in _architecture.Items)
 		{
 			if ((string) i.Content != project.Architecture) continue;
@@ -36,4 +43,26 @@ public class ProjectSettings : Window
 		var arch = (string) ((ComboBoxItem) _architecture.SelectedItem!).Content;
 		model.Architecture = arch!;
 	}
+
+	#region Nuget
+
+	
+	
+	private async void InputElement_OnKeyUp(object? sender, KeyEventArgs e)
+	{
+		if(e.Key != Key.Return) return;
+		
+		_packageList.Children.Clear();
+		_packageList.Children.Add(new TextBlock{Text = "Searching...", FontSize = 24, 
+			TextAlignment = TextAlignment.Center, Margin = new Thickness(16)});
+		
+		var model = (ProjectModel) DataContext!;
+		var results = await Nuget.Search(_packageSearch.Text);
+
+		_packageList.Children.Clear();
+		foreach (var package in results)
+			_packageList.Children.Add(await PackageEntry.Create(model, package));
+	}
+
+	#endregion
 }
