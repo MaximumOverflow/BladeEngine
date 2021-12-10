de49e29695e1e0a25a98fa36dae6525d94d627cf
Massimo Bertozzi <maximumoverflow904@gmail.com>
2021-12-10 18:19:33 +0100

    NuGet searches can now return multiple packages. Fixed scrolling in the package manager.

--- Editor/NuGet/Search.cs
+++ Editor/NuGet/Search.cs
@@ -1,7 +1,6 @@
-﻿using NuGet.Common;
+﻿using NuGet.Protocol.Core.Types;
 using NuGet.Configuration;
-using NuGet.Protocol.Core.Types;
-using NuGet.Versioning;
+using NuGet.Common;
 
 namespace BladeEngine.Editor.NuGet;
 
@@ -9,62 +8,44 @@ public static partial class Nuget
 {
 	// private static readonly PackageMetadataResource MetadataResource = CreateMetadataResource();
 
-	private static Task<PackageMetadataResource> CreateMetadataResource()
+	private static Task<PackageSearchResource> CreateMetadataResource()
 	{
 		var providers = new List<Lazy<INuGetResourceProvider>>();
 		providers.AddRange(Repository.Provider.GetCoreV3());
 		var source = new PackageSource("https://api.nuget.org/v3/index.json");
 		var repository = new SourceRepository(source, providers);
-		return repository.GetResourceAsync<PackageMetadataResource>();
+		return repository.GetResourceAsync<PackageSearchResource>();
 	}
 
 	public static async Task<IEnumerable<NugetSearchResult>> Search(string search)
 	{
 		Debug.Log($"Searching NuGet for '{search}'.");
-		var dict = new Dictionary<string, (IPackageSearchMetadata, List<NuGetVersion>)>();
 
 		try
 		{
 			var repo = await CreateMetadataResource();
-			var task = await repo.GetMetadataAsync(search, false, false, 
-				NullSourceCacheContext.Instance, NullLogger.Instance, CancellationToken.None);
-			
+			var results = await repo.SearchAsync(search, new SearchFilter(false), 
+				0, 32, NullLogger.Instance, CancellationToken.None);
+
 			Debug.Log($"NuGet search for '{search}' complete.");
-			
-			foreach (var metadata in task)
-			{
-				if (!dict.ContainsKey(metadata.Title))
-				{
-					var package = metadata;
-					var versions = new List<NuGetVersion>{metadata.Identity.Version};
-					dict.Add(metadata.Title, (package, versions));
-				}
-				else
-				{
-					var data = dict[metadata.Title];
-					if (metadata.Identity.Version.CompareTo(data.Item1.Identity.Version) > 0)
-						data.Item1 = metadata;
-					data.Item2.Add(metadata.Identity.Version);
-					dict[metadata.Title] = data;
-				}
-			}
-			
-			Debug.Log($"Found {dict.Count} {(dict.Count == 1 ? "result" : "results")} for '{search}'.");
+
 			
 			var packages = new List<NugetSearchResult>();
-			foreach (var (package, versions) in dict.Values)
+			foreach (var package in results)
 			{
-				versions.Sort((a, b) => -a.CompareTo(b));
-				var ver = versions.Select(v => v.ToString()).ToArray();
-			
+				var versions = (await package.GetVersionsAsync()).Select(v => v.Version.ToString()).ToList();
+				versions.Reverse();
+				
 				packages.Add(new NugetSearchResult
 				(
-					package.Title, package.Owners, package.Authors, package.Description, package.DownloadCount, ver,
+					package.Title, package.Owners, package.Authors, package.Description, package.DownloadCount, versions,
 					package.IconUrl?.ToString(), package.LicenseUrl?.ToString(), package.ReadmeUrl?.ToString(), 
 					package.ProjectUrl?.ToString(), package.PackageDetailsUrl?.ToString()
 				));
 			}
 			
+			Debug.Log($"Found {packages.Count} {(packages.Count == 1 ? "package" : "packages")} for '{search}'.");
+
 			return packages;
 		}
 		catch (Exception e)
